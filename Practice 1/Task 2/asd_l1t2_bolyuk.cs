558029d73bc8279e9ad43e12bb363f6323af3a91
bolyuk <90848724+bolyuk@users.noreply.github.com>
2021-10-20 19:39:42 +0300

    edit second task

--- Practice 1/Task 2/asd_l1t2_bolyuk.cs
+++ Practice 1/Task 2/asd_l1t2_bolyuk.cs
@@ -1,16 +1,16 @@
 using System;
 class asd_l1t2_bolyuk
 {
-    static void Main()     
+    static void Main()
     {
         int count = 0;
         for (int Y = 1582; Y <= 4902; Y++)
         {
             int y = Y % 1000;
             int c = Y / 1000;
-          for(int m=1; m<=12;m++)
+            for (int m = 1; m <= 12; m++)
             {
-                if (((2.6 * m - 0.2) + 13 + y + y / 4 + c / 4 - 2 * c) % 7 == 5) count++;
+                if ((Math.Round(2.6 * m - 0.2) + 13 + y + Math.Round(y / 4.0) + Math.Round(c / 4.0) - 2 * c) % 7 == 5) count++;
             }
         }
         Console.WriteLine("Кількість п'ятниць 13: " + count.ToString());
