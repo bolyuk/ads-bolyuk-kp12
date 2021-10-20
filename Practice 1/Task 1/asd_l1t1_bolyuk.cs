7af7a901e25a8530b6db259a0242a6e56613b762
bolyuk <90848724+bolyuk@users.noreply.github.com>
2021-10-20 23:03:29 +0300

    edit 1 task

--- Practice 1/Task 1/asd_l1t1_bolyuk.cs
+++ Practice 1/Task 1/asd_l1t1_bolyuk.cs
@@ -8,10 +8,23 @@ class asd_l1t1_bolyuk
         Console.WriteLine("input x:"); x = Double.Parse(Console.ReadLine());
         Console.WriteLine("input y:"); y = Double.Parse(Console.ReadLine());
         Console.WriteLine("input z:"); z = Double.Parse(Console.ReadLine());
+        if(x * x + x * y * y + x * x * z > 0)
+        {
+            a = 1.0 + (Sin(x) / Sqrt(x * x + x * y * y + x * x * z));
+            if(y + z / a > 0 && x + (a / (y + z / a)) > 0)
+            {
+                b = Log(a * a * a + x * x) / (x + (a / (y + z / a)));
+                Console.WriteLine("a=" + a.ToString() + "\nb=" + b.ToString());
+            } else
+            {
+                Console.WriteLine("Incorect value!");
+            }
+        } else
+        {
+            Console.WriteLine("Incorect value!");
+        }
 
-        a = 1.0 + (Sin(x) / Sqrt(x * x + x * y * y + x * x * z));
-        b = Log(a * a * a + x * x) / (x + (a/(y+z/a)));
-        Console.WriteLine("a="+a.ToString()+"\nb="+b.ToString());
+            
     }
 
 }
