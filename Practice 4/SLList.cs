e990c80f0e7339d09e846ec8e84822383b82fd7a
bolyuk <90848724+bolyuk@users.noreply.github.com>
2021-12-27 14:49:11 +0200

    Лаб4 завершена

---
+++ Practice 4/SLList.cs
@@ -0,0 +1,80 @@
+﻿class SLList
+{
+    public Node tail;
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+        public Node(int data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    public SLList(int data)
+    {
+        tail = new Node(data);
+        tail.next = tail;
+    }
+
+public void AddFirst(int data) 
+    { 
+        tail.next = new Node(data, tail.next); 
+    }
+
+    public void AddToPosition(int data, int position) {
+        Node buf = tail.next;
+        for (int i=0; i < position; i++)
+            buf = buf.next;
+        buf.next = new Node(data,buf.next);
+    }
+    public void AddLast(int data)
+    {
+        Node buf = tail.next;
+        tail.next = new Node(data, buf);
+    }
+
+    public void DeleteFirst() {
+        tail.next = tail.next.next;
+    }
+    public void DeleteFromPosition(int position) {
+        int i = -1;
+        Node buf = tail;
+        while (i < position)
+            buf = buf.next;
+        buf.next = buf.next.next;
+    }
+    public void DeleteLast()
+    {
+        Node current = tail.next;
+        while (current.next != tail.next)
+        {
+            current = current.next;
+        }
+        current.next = tail.next;
+    }
+
+    public void Print() {
+        string result = "";
+        Node buf = tail.next;
+        for(int i=0;  buf != tail.next || i==0; i++)
+        {
+            result +="("+i+") "+ buf.data + " ";
+            buf = buf.next;
+        }
+        System.Console.WriteLine(result);
+    }
+
+    public int Get(int i)
+    {
+        int j = -1;
+        Node buf = tail;
+        while (j < i)
+            buf = buf.next;
+        return buf.data;
+    }
+}
---
+++ Practice 4/asd_l4t1_bolyuk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+    class asd_l4t1_bolyuk
+{
+
+     static void  Main()
+    {
+        SLList list = new SLList(1);
+        for (int i = 0; i < 10; i++)
+            list.AddLast(new Random().Next(-50, 50));
+
+        list.Print();
+
+        int maxData=list.tail.next.data, max=0;
+        SLList.Node buf = list.tail.next;
+        for (int i=0; buf != list.tail.next || i==0; i++)
+        {
+            if (maxData < buf.data)
+            {
+                max = i;
+                maxData = buf.data;
+            }
+            buf = buf.next;
+        }
+        Console.WriteLine("max pos is "+max);
+        list.AddToPosition(new Random().Next(-50, 50), max);
+        list.Print();
+
+    }
+
+    }
+
